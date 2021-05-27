aa6f6fa9eb75b73d5ffbdb4f8784741e2a450304
stiven212 <stivfunk2000@hotmail.com>
2021-05-26 23:51:10 -0500

    Single page

--- flutter_application_1/lib/src/pages/single_character_page.dart
+++ flutter_application_1/lib/src/pages/single_character_page.dart
@@ -1,8 +1,24 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/models/char.dart';
 
 class SingleCharacter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Character'),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              // Image(image: Characters[])
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
